4bd1d0b7d66b188757b39d556ba441e413478409
lee <dlwnddnjs48@gmail.com>
2022-04-06 06:53:09 +0900

    중복제거 메소드 구현

--- Linked_List/test.java
+++ Linked_List/test.java
@@ -3,9 +3,17 @@ package Linked_List;
 public class test {
 
 	public static void main(String[] args) {
-		Node A = new Node(1);
+		LinkedList A = new LinkedList();
 		A.append(2);
+		A.append(1);
 		A.append(2);
+		A.append(3);
+		A.append(4);
+		A.append(4);
+		A.append(2);
+		A.print();
+		System.out.println("---------");
+		A.removeDups();
 		A.print();
 	}
 
