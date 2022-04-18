61d2ccded9f30f40b7b81db25a6cc9b6f708aba3
lee <dlwnddnjs48@gmail.com>
2022-04-18 17:40:53 +0900

    x 기준으로 값을 좌우로 나누는 기능

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -2,55 +2,109 @@ package Linked_List;
 
 class LinkedList{
 	Node header;
-	static class Node{
+	
+	class Node{	
 		int data;
-		Node next = null;
+		Node next = null;	
 	}
+	
 	LinkedList(){
-		header = new Node();
+		header = new Node();	
 	}
+	
     void append(int d) {
+    	
         Node end = new Node();
         end.data = d;
         Node n = header;
+        
         while (n.next != null){
             n = n.next;
         }
+        
         n.next = end;
     }
     
     void delete(int d){
+    	
     	Node n = header;
+    	
     	while(n.next != null) {
+    		
     		if(n.next.data == d) {
     			n.next = n.next.next;
     		}else {
     			n = n.next;
     		}
+    		
     	}
+    	
     }
     
     void print() {
+    	
     	Node n = header.next;
+    	
+    	if(n == null) {
+    		return;
+    	}
+    	
     	while(n.next != null) {
-    		System.out.println(n.data);
+    		System.out.print(n.data + " -> ");
     		n = n.next;
     	}
+    	
     	System.out.println(n.data);
+    	
     }
     
     void removeDups() {
+    	
     	Node n = header;
+    	
     	while (n != null && n.next != null) {
     		Node r = n;
+    		
     		while (r.next != null) {
+    			
     			if(n.data == r.next.data) {
     				r.next = r.next.next;
     			}else {
     				r = r.next;
     			}
+    			
     		}
+    		
     		n = n.next;
     	}
+    	
     }
+    
+    Node get(int d) {
+    	
+    	Node n = header;
+    	
+    	for(int i=0; i<d; i++) {
+    		n = n.next;
+    	}
+    	return n;
+    	
+    }
+    
+    void deleteIndex(Node n) {
+    	
+    	if(n == null || n.next == null) {
+    		return;
+    	}
+    	
+    	Node next = n.next;
+    	n.data = next.data;
+    	n.next = next.next;
+    	
+    }
+    
+    void reset() {
+    	header = new Node(); 
+    }
+    
 }
--- Linked_List/test.java
+++ Linked_List/test.java
@@ -1,20 +1,135 @@
 package Linked_List;
+import Linked_List.LinkedList.Node;
 
 public class test {
 
 	public static void main(String[] args) {
-		LinkedList A = new LinkedList();
-		A.append(2);
-		A.append(1);
-		A.append(2);
-		A.append(3);
-		A.append(4);
-		A.append(4);
-		A.append(2);
-		A.print();
-		System.out.println("---------");
-		A.removeDups();
-		A.print();
+			
+//코드 테스트
+		Runtime.getRuntime().gc();
+		long sTime = System.currentTimeMillis();
+		
+		try {
+			
+			LinkedList A = new LinkedList();
+			A.append(7);
+			A.append(2);
+			A.append(8);
+			A.append(5);
+			A.append(3);
+			A.append(4);
+			A.print();
+			
+//			//정수 n값 삭제
+//			A.delete(2);	
+//			A.print();
+//			System.out.println("------------------");
+//			
+//			//중간노드중 n번째 인덱스 삭제
+//			A.deleteIndex(A.get(4));
+//			A.print();
+//			System.out.println("------------------");
+			
+			//n값 기준으로 작은숫자는 왼쪽 큰 숫자는 오른쪽으로 나누기
+			Node n = partition2(A.get(1), 5);
+			while(n.next != null) {
+				System.out.print(n.data + " -> ");
+				n = n.next;
+			}
+			System.out.println(n.data);
+			
+			
+			} catch (Exception e) { 
+				e.printStackTrace(); 
+		} 
+		
+		long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("사용된 메모리 --- " + used + " bytes");
+		Double sec = (System.currentTimeMillis() - sTime) / 1000.0;
+		System.out.printf("소요시간 --- (%.2f초)%n", sec);
+		
+		
 	}
+	
+	//4포인터 파티션
+	 static Node partition1(Node n, int x) {
+	    	
+	    	Node start1 = null; 
+	    	Node end1 = null;
+	    	Node start2 = null;
+	    	Node end2 = null;
+	    	
+	    	while(n != null) {
+	    		
+	    		Node next = n.next; //다음 노드의 위치를 따로 저장
+	    		n.next = null; // n값만 필요하기 떄문에 n에 연결된 다음 데이터는 삭제
+	    		
 
+         
+
+	    		
+	    		
+	    		if(n.data < x) {
+	    			
+	    			if(start1 == null) {
+	    				start1 = n;
+	    				end1 = start1;
+	    			}else {
+	    				end1.next = n;
+	    				end1 = n;
+	    			}
+	    			
+	    		}else {
+	    			
+	    			if(start2 == null) {
+	    				start2 = n;
+	    				end2 = start2;
+	    			}else {
+	    				end2.next = n;
+	    				end2 = n;
+	    			}
+	    			
+	    		}
+	    		
+	    	      
+	    		
+	    		n = next;
+	    		
+	    	}
+	    	
+	    	if(start1 == null) {
+	    		return start2;
+	    	}
+	    	
+	    	end1.next = start2;
+	    	return start1;
+	    	
+	    }
+	 
+	//4포인터 파티션 개선 ---> 2포인터
+	 static Node partition2(Node n, int x) {
+		 
+		 Node head = n;
+		 Node tail = n;
+		 
+		 while(n != null) {
+			 
+			 Node next = n.next;
+			 
+			 if(n.data < x ) {
+				n.next = head; 
+				head = n;
+			 }else {
+				 tail.next = n;
+				 tail = n;
+			 }
+			 
+			 n = next;
+			 
+		 }
+		 
+		 tail.next = null;
+		 return head;
+	 }
+	 
 }
